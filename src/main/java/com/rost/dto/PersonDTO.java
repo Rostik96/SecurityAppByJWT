a6796d8a3f61040d2f86c9f44e0e3df74c2f0c2d
Rost <coderost@yandex.ru>
2023-02-12 17:34:18 +0300

    Create DTO for entity.

--- src/main/java/com/rost/dto/PersonDTO.java
+++ src/main/java/com/rost/dto/PersonDTO.java
@@ -1,9 +1,8 @@
 package com.rost.dto;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
-
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 

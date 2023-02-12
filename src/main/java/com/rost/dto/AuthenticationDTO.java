8c921622c74cf0ae0e2638e5e2ac997df46b9dd1
Rost <coderost@yandex.ru>
2023-02-12 17:35:24 +0300

    Refactor DTO. Refactor property file's template.

--- src/main/java/com/rost/dto/AuthenticationDTO.java
+++ src/main/java/com/rost/dto/AuthenticationDTO.java
@@ -11,6 +11,5 @@ public class AuthenticationDTO {
     @NotEmpty(message = "Имя не должно быть пустым")
     @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов")
     private String username;
-
     private String password;
 }

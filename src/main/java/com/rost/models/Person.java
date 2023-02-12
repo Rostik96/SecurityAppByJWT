2d11c46505c324867aafdcf1ef5cd0421f9bb988
Rost <coderost@yandex.ru>
2023-02-12 18:12:13 +0300

    Create person-entity.

--- src/main/java/com/rost/models/Person.java
+++ src/main/java/com/rost/models/Person.java
@@ -2,31 +2,33 @@ package com.rost.models;
 
 
 import com.rost.security.Role;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 import lombok.ToString;
 
 @Entity
-@NoArgsConstructor
-@Getter
-@Setter
-@ToString
+@Data
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private long id;
 
     @NotEmpty(message = "Имя не должно быть пустым")
     @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов")
-    @Column(name = "username")
     private String username;
 
     @Min(value = 1900, message = "Год рождения должен быть ≥ 1904") //Люсиль Рэндон, Франция
     private int yearOfBirth;
 
+    @ToString.Exclude
     private String password;
 
     @Enumerated(EnumType.STRING)
